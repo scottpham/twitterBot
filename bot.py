cc9b40f1f5c85ad1aec0e33a35a3aaeb29ba7b25
Amy <amy.mae.lee@gmail.com>
2014-06-01 02:39:50 -0700

    Completed ajax call

---
+++ bot.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect, session, url_for, flash
+import tweet
+import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    # tweet.tweet("hi!!")
+    return render_template("index.html")
+
+@app.route("/", methods=["POST"])
+def tweet_data():
+    body = request.json
+    data = body['data']
+    columns = body['columns']
+    text_inserts = body['text_inserts']
+    tweet.tweet(data, columns, text_inserts)
+    return redirect(url_for("index"))
+
+if __name__ == "__main__":
+    app.run(debug = True)
---
+++ tweet.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+# https://code.google.com/p/python-twitter/
+# dev.twitter.com
+
+import sys
+import os
+from os.path import exists
+from twython import Twython
+import time
+
+
+def tweet(data, columns, text_inserts):
+    API_KEY = 'SviKWezy1ohjALJG91exYJ1qn'
+    API_SECRET = 'MazAwFFYV3XNLs0aWMPuW3q9jrWSK1IS0E5Nabl8RyLX8EHfGq'
+    ACCESS_TOKEN = '2538298806-UrVB8auRC9452OLGf7pEHAqs08rJI9XbFk15EA7'
+    ACCESS_TOKEN_SECRET = 'PHWwXSLdHEogzX4S7CXHwFPb0FudtZajOcDlR2KhaKqLz'
+ 
+    twitter = Twython(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+    text_segment_length = len(columns) + len(text_inserts)
+    
+    # print "this is the full data: %r" % data
+    # print "this is the full columns: %r" % columns
+    # print "this is the full text: %r" % text_inserts
+
+    for row in data:
+        text = ""
+        # print row
+        # print "this is one row: %r" % row 
+        for num in range(text_segment_length):
+            if columns.get(str(num)):
+                text += row[columns[str(num)]]
+                # print text 
+            else:
+                # print num
+                text += text_inserts[str(num)]
+                # print text
+
+        # print text
+        twitter.update_status(status=text)
+        time.sleep(5)
+
+def main():
+    tweet("hi again")
+    print "tweeted successfully"
+
+if __name__ == "__main__":
+    main()
